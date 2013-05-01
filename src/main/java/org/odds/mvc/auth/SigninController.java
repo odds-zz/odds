1cdc7d703f18722b90b2fa7290dfa51e76ead8a5
Kenneth Kataiwa <kenkataiwa@gmail.com>
2013-05-01 23:00:20 +0300

    Signin controller

--- src/main/java/org/odds/mvc/auth/SigninController.java
+++ src/main/java/org/odds/mvc/auth/SigninController.java
@@ -8,20 +8,9 @@ package org.odds.mvc.auth;
  *
  * @author kenkataiwa
  */
-import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.odds.hibernate.entities.User;
-import org.odds.hibernate.dao.UserDAO;
-import org.odds.mvc.extensions.ajax.AjaxUtils;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 /**
