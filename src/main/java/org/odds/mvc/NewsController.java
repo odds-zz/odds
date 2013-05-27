7c45aa5af78f7fd3bdfbf0bd81711174f56ea66d
Kenneth Kataiwa <kenkataiwa@gmail.com>
2013-05-28 01:29:00 +0300

    Adding news dao, controllers, and validator

--- src/main/java/org/odds/mvc/NewsController.java
+++ src/main/java/org/odds/mvc/NewsController.java
@@ -8,6 +8,9 @@ package org.odds.mvc;
  *
  * @author kenkataiwa
  */
+import java.util.List;
+import org.odds.hibernate.dao.NewsDAO;
+import org.odds.hibernate.entities.News;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +24,8 @@ public class NewsController {
     @RequestMapping(value = "/news")
     public String index(Model model) {
 
+        List<News> nList = NewsDAO.listNews();
+        model.addAttribute("news", nList);
         return "news/index";
     }
 
@@ -29,5 +34,4 @@ public class NewsController {
 
         return "news/orphanages";
     }
-
 }
--- src/main/java/org/odds/mvc/admin/news/CreateNewsController.java
+++ src/main/java/org/odds/mvc/admin/news/CreateNewsController.java
@@ -7,9 +7,15 @@ package org.odds.mvc.admin.news;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import org.odds.hibernate.dao.NewsDAO;
+import org.odds.hibernate.entities.News;
+import org.odds.mvc.admin.form.NewsValidator;
+import org.odds.mvc.admin.form.OrphanageBean;
+import org.odds.mvc.admin.form.PostNewsBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,21 +29,37 @@ import org.springframework.web.bind.support.SessionStatus;
 @RequestMapping("/admin/news/create")
 public class CreateNewsController {
 
+    NewsValidator newsValidator;
+
+    @Autowired
+    public CreateNewsController(NewsValidator newsValidator) {
+        this.newsValidator = newsValidator;
+    }
+
     @RequestMapping(method = RequestMethod.GET)
     public String initForm(Model model) {
+        PostNewsBean news = new PostNewsBean();
+        model.addAttribute("news", news);
 
         return "/admin/news/create";
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public String processSubmit(
+            @ModelAttribute("news") PostNewsBean form,
             BindingResult result, SessionStatus status, Model model) {
 
+        newsValidator.validate(form, result);
 
         if (result.hasErrors()) {
             //if validator failed
             return "/admin/news/create";
         } else {
+            News news = new News();
+            news.setTitle(form.getTitle());
+            news.setBody(form.getBody());
+            NewsDAO.createNews(news);
+
             status.setComplete();
             //form success
             return "/admin/news/create";
