e1e76fa11e74b38cc74d6dd564752bd17439fe35
Matei <Matei@Matnet>
2013-06-18 18:34:06 +0300

    children count

--- src/main/java/org/odds/hibernate/dao/ChildrenDAO.java
+++ src/main/java/org/odds/hibernate/dao/ChildrenDAO.java
@@ -143,7 +143,28 @@ public class ChildrenDAO {
     }
 
     public static Integer countChildren() {
+        Transaction tx = null;
+        Session session;
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        int count = 0;
+        try {
+            tx = session.beginTransaction();
+            Query q;
+            q = session.createQuery("select count(*) from Children");
+            count = ((Long) q.uniqueResult()).intValue();
+            tx.commit();//end of transaction
+            Connection close; //end of  session
+            close = session.close();
+        } catch (RuntimeException e) {
+            if (tx != null && tx.isActive()) {
+                try {
+                    tx.rollback();
+                } catch (HibernateException he) {
+                    System.out.println("Error rolling back the Transaction " + he.toString());
+                }
+            }
 
-        return 104;
+        }
+          return count;
     }
 }
